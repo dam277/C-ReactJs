5ff88aba47e36b09b86ac874f9f8f22c186b9327
dam277 <damis.loup@gmail.com>
2023-05-25 12:29:04 +0200

    Chap 5 finished

---
+++ 02-SourceCode/filRouge/shiny-agency/src/Components/Card/index.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types'
+import DefaultPicture from '../../Assets/profile.png'
+
+function Card({ label, title, picture }) {
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column', padding: 15 }}>
+            <span>{label}</span>
+            <img src={picture} alt="freelance" height={80} width={80} />
+            <span>{title}</span>
+        </div>
+    )
+}
+
+Card.propTypes = {
+    label: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
+}
+
+Card.defaultProps = {
+    label: '',
+    title: '',
+    picture: DefaultPicture,
+}
+ 
+export default Card
--- 02-SourceCode/filRouge/shiny-agency/src/Pages/Freelances/index.jsx
+++ 02-SourceCode/filRouge/shiny-agency/src/Pages/Freelances/index.jsx
@@ -1,7 +1,31 @@
-function Freelances() {
+import DefaultPicture from '../../Assets/profile.png'
+import Card from '../../Components/Card';
+
+const freelanceProfiles = [
+    {
+        name: 'Jane Doe',
+        jobTitle: 'Devops',
+    },
+    {
+        name: 'John Doe',
+        jobTitle: 'Developpeur frontend',
+    },
+    {
+        name: 'Jeanne Biche',
+        jobTitle: 'Développeuse Fullstack',
+    },
+]
+
+
+function Freelances() 
+{
     return (
         <div>
             <h1>Freelances 👩‍💻👨‍💻👩‍💻</h1>
+            {freelanceProfiles.map((profile, index) => 
+            (
+                <Card key={`${profile.name}-${index}`}  label={profile.jobTitle} picture={profile.picture} title={profile.name}/>
+            ))}
         </div>
     );
 }
